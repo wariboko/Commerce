d7afd13fcc5d65635736869e841caa6b7343186e
wariboko <davis.waris@gmail.com>
2021-02-27 04:34:12 +0200

    latest

--- commerce/settings.py
+++ commerce/settings.py
@@ -78,6 +78,10 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'USER': 'sqlite3',
+        'PASSWORD': 'sqlite3',
+        'HOST': 'db',
+        'PORT': 5432,
     }
 }
 
